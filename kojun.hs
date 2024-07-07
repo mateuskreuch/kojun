ae4df0073ea5d0f321220d9df2f437726327c2d3
Mateus A. Kreuch <mateus.kreuch@gmail.com>
2024-07-07 05:23:24 -0300

    improved haskell

--- kojun.hs
+++ kojun.hs
@@ -17,16 +17,9 @@ regions = newRegionMatrix (puzzleW, puzzleH)
    ,"l","m","n","n","n","o","p","j"
    ,"m","m","m","m","q","o","o","o"
    ,"r","q","q","q","q","o","s","s"]
-regionCoords =
-   let
-      mapRegions (x, y) dict
-         | y > puzzleH = dict
-         | x > puzzleW = mapRegions (1, y + 1) dict
-         | otherwise =
-            mapRegions (x + 1, y) (Map.insertWith (++) (getRegion (x, y))
-                                                       [(x, y)] dict)
-   in do
-      mapRegions (1, 1) Map.empty
+
+regionCoords = Map.fromListWith (++)
+   [(getRegion (x, y), [(x, y)]) | y <- [1..puzzleH], x <- [1..puzzleW]]
 
 --------------------------------------------------------------------------------
 
@@ -53,50 +46,45 @@ main = do
 solve puzzle (x, y)
    | y > puzzleH = return True
    | x > puzzleW = solve puzzle (1, y + 1)
-   | otherwise =
-      let    
-         tryNumbers puzzle (x, y) [] = return False
-         tryNumbers puzzle (x, y) (guess:rest) = do  
-            setPuzzle puzzle (x, y) guess
- 
-            solution <- solve puzzle (x + 1, y)
+   | otherwise = do
+      value <- getPuzzle puzzle (x, y)
 
-            if solution then
-               return True
-            else
-               setPuzzle puzzle (x, y) 0 >> tryNumbers puzzle (x, y) rest
-      in do
-         value <- getPuzzle puzzle (x, y)
+      if value == 0 then
+         getAvailableNumbers puzzle (x, y) >>= tryNumbers puzzle (x, y)
+      else
+         solve puzzle (x + 1, y)
 
-         case value of
-            0 -> getAvailableNumbers puzzle (x, y) >>= tryNumbers puzzle (x, y)
-            _ -> solve puzzle (x + 1, y)
+tryNumbers puzzle (x, y) [] = return False
+tryNumbers puzzle (x, y) (guess:rest) = do
+   setPuzzle puzzle (x, y) guess
+
+   solution <- solve puzzle (x + 1, y)
+
+   if solution then
+      return True
+   else
+      setPuzzle puzzle (x, y) 0 >> tryNumbers puzzle (x, y) rest
 
 --------------------------------------------------------------------------------
 
-getAvailableNumbers p (x, y) = do
-   a <- getOrthogonalNumbers p (x, y)
-   b <- getRegionNumbers p (x, y)
-   c <- getVerticalNumbers p (x, y)
+getAvailableNumbers puzzle (x, y) = do
+   a <- getOrthogonalNumbers puzzle (x, y)
+   b <- getRegionNumbers puzzle (x, y)
+   c <- getVerticalNumbers puzzle (x, y)
 
    return (c \\ (a `union` b))
 
-getOrthogonalNumbers p (x, y) =
-   mapM (getPuzzle p) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+getOrthogonalNumbers puzzle (x, y) =
+   mapM (getPuzzle puzzle) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
 
-getRegionNumbers p (x, y) = mapM (getPuzzle p) (getRegionCoords (x, y))
+getRegionNumbers puzzle (x, y) =
+   mapM (getPuzzle puzzle) (getRegionCoords (x, y))
 
-getVerticalNumbers p (x, y) =
-   let
-      getRegionSize coord = length (getRegionCoords coord)
-      inSameRegion a b = inbounds a && inbounds b && getRegion a == getRegion b
-      tryGetRegionNeighbor a b fallback | inSameRegion a b = getPuzzle p b
-                                        | otherwise = return fallback
-   in do
-      a <- tryGetRegionNeighbor (x, y) (x, y + 1) 0
-      b <- tryGetRegionNeighbor (x, y) (x, y - 1) (getRegionSize (x, y) + 1)
+getVerticalNumbers puzzle (x, y) = do
+   a <- tryGetRegionNeighbor puzzle (x, y) (x, y + 1) 0
+   b <- tryGetRegionNeighbor puzzle (x, y) (x, y - 1) (getRegionSize (x, y) + 1)
 
-      return [(a + 1)..(b - 1)]
+   return [(a + 1)..(b - 1)]
 
 --------------------------------------------------------------------------------
 
@@ -106,20 +94,26 @@ getRegion coord = regions ! coordToIndex coord
 
 getRegionCoords coord = regionCoords Map.! getRegion coord
 
+getRegionSize coord = length (getRegionCoords coord)
+
+inSameRegion a b = inbounds a && inbounds b && getRegion a == getRegion b
+
 newPuzzleMatrix (w, h) = newListArray (1, w * h)
 
 getPuzzle :: Num e => IOArray Int e -> (Int, Int) -> IO e
-getPuzzle p coord | inbounds coord = readArray p (coordToIndex coord)
-                  | otherwise = return 0
+getPuzzle puzzle coord | inbounds coord = readArray puzzle (coordToIndex coord)
+                       | otherwise = return 0
 
-setPuzzle p coord = writeArray p (coordToIndex coord)
+setPuzzle puzzle coord = writeArray puzzle (coordToIndex coord)
 
-printPuzzle p =
-   let
-      spot x = (putStr . show) x >> putStr " "
-      line y = mapM_ (\x -> getPuzzle p (x, y) >>= spot) [1..puzzleW]
-   in
-      mapM_ (\y -> line y >> putStr "\n") [1..puzzleH]
+tryGetRegionNeighbor puzzle a b fallback | inSameRegion a b = getPuzzle puzzle b
+                                         | otherwise = return fallback
+
+printPuzzle puzzle =
+   mapM_ (\y ->
+      mapM_ (\x -> getPuzzle puzzle (x, y) >>= putStr . (++ " ") . show)
+      [1..puzzleW] >> putStrLn "")
+   [1..puzzleH]
 
 inbounds (x, y) = x >= 1 && x <= puzzleW && y >= 1 && y <= puzzleH
 
